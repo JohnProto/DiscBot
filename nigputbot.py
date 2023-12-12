da83783f1da38b7ee141e17c4e321459815372c3
John <gproto03@gmail.com>
2023-12-12 04:36:30 +0200

    Add files via upload
    finally fixed on_voice_state_update

--- nigputbot.py
+++ nigputbot.py
@@ -48,6 +48,10 @@ async def on_message(message):
             await message.channel.send(f'{target_user.mention} :foot:')
             break
 
+    target_user_id = 949241373922570240
+    if message.author.id == target_user_id:
+        await message.channel.send(f'stfu im better')
+
 async def play_audio_with_delay(audio_source, delay):
     await asyncio.sleep(delay)
     voice_client.play(audio_source)
@@ -57,22 +61,24 @@ async def on_voice_state_update(member, before, after):
     global voice_client
 
     guild = member.guild
-    voice_channel = discord.utils.get(guild.channels, id=1182016043628105811, type=discord.ChannelType.voice)
+    target_channel_id = 1182016043628105811
+    voice_channel = guild.get_channel(target_channel_id)    
     audio_source = FFmpegPCMAudio('Erika.mp3')
 
     bot_voice = client.voice_clients
-    if voice_channel and member.id != client.user.id:
-        if len(voice_channel.members) > 0 and not bot_voice:   
-            voice_client = await voice_channel.connect()
-            await play_audio_with_delay(audio_source, 1)
-            print(f"Bot joined {voice_channel.name}")
-        elif len(voice_channel.members) > 1 and bot_voice:
-            if not before.channel and after.channel and bot_voice:
-                if member.id == 361434482735054850:
-                    audio_source = FFmpegPCMAudio('forgor.mp3')
-                await play_audio_with_delay(audio_source, 1.3)
-        elif len(voice_channel.members) == 1 and bot_voice:
-            await bot_voice[0].disconnect()
-            print(f"bot left {voice_channel.name}")
+    if (before.channel and before.channel.id == target_channel_id and after.channel == None) or (after.channel and after.channel.id == target_channel_id):
+        if voice_channel and member.id != client.user.id:
+            if len(voice_channel.members) > 0 and not bot_voice:   
+                voice_client = await voice_channel.connect()
+                await play_audio_with_delay(audio_source, 1)
+                print(f"Bot joined {voice_channel.name}")
+            elif len(voice_channel.members) > 1 and bot_voice:
+                if not before.channel and after.channel and bot_voice:
+                    if member.id == 361434482735054850:
+                        audio_source = FFmpegPCMAudio('forgor.mp3')
+                    await play_audio_with_delay(audio_source, 1.3)
+            elif len(voice_channel.members) == 1 and bot_voice:
+                await bot_voice[0].disconnect()
+                print(f"bot left {voice_channel.name}")
 
 client.run('MTE4MjM5OTY3Njc1MDc3MDI4Ng.GETzSz.VJQKqwK9peVDVG0VRqmP6Jgy0OYQbOOS8cuOGI')
